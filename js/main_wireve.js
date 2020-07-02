fcca33ca3a6fe51e54c5219d9a94c3b569363500
majadc <maja.chabros@gmail.com>
2020-07-02 10:20:53 +0200

    basic structure

---
+++ js/main_wireve.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+  $('body').scrollspy({ target: '#navbarMain'});
+      $('#navbarMain a').on('click', function (e) {
+        if (this.hash !== '') {
+          e.preventDefault();
+          const hash = this.hash;
+          const heightNavbar = $("#navbarMain").height();
+          const marginSection = parseInt($(`${$(this).attr('href')}`).css('margin-top'));
+          let offset = $(hash).offset().top - heightNavbar - marginSection;
+          $('html, body').animate({
+            scrollTop: offset
+          }, 900, function () {
+            if ( history.pushState ) {
+              history.pushState(null, null, hash);
+            } else {
+              location.hash = hash;
+            }
+          
+          });
+        }
+      });
+
+
+  
+  
+      $('#mdc-stats .counter').counterUp({
+        duration: 1000,
+        delay: 16
+      });
+
+      $("#clients .owl-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+          0: {
+            items: 2
+          },
+          768: {
+            items: 4
+          },
+          900: {
+            items: 6
+          }
+        }
+      });
+
+
+
+  
+});
