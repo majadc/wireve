0ab63f3c73c4d85f2ab33851dcc9c1628041f4a1
majadc <maja.chabros@gmail.com>
2020-07-10 10:26:20 +0200

    gallery

---
+++ js/wireve_main.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function() {
+  $('body').scrollspy({ target: '#navbarMain'});
+      $('#navbarMain a').on('click', function (e) {
+        if (this.hash !== '') {
+          e.preventDefault();
+          const hash = this.hash;
+          const heightNavbar = $("#navbarMain").height();
+          const marginSection = parseInt($(`${$(this).attr('href')}`).css('margin-top'));
+          let offset = $(hash).offset().top - heightNavbar - marginSection;
+          $('html, body').animate({
+            scrollTop: offset
+          }, 900, function () {
+            if ( history.pushState ) {
+              history.pushState(null, null, hash);
+            } else {
+              location.hash = hash;
+            }
+          
+          });
+        }
+      });
+
+
+  
+  
+      $('#mdc-stats .counter').counterUp({
+        duration: 1000,
+        delay: 16
+      });
+
+      $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+          0: {
+            items: 2
+          },
+          768: {
+            items: 4
+          },
+          900: {
+            items: 6
+          }
+        }
+      });
+
+  let delay = 1;
+  $("#mdc-awards").find(".mdc-awards__item").each(function(){
+    $(this).find('.mdc-awards__img-ico').css('animation-delay', `${delay}s`);
+    $(this).find('.mdc-awards__img img').css('animation-delay', `${delay}s`);
+    delay++;
+  });
+  
+  
+
+  //comments accordion
+
+  $('.accordionQuestions-panel').on('show.bs.collapse', function () {
+   
+   $(this).find('.mdc-questions__header').addClass('active');
+  })
+  $('.accordionQuestions-panel').on('hide.bs.collapse', function () {
+   
+    $(this).find('.mdc-questions__header').removeClass('active');
+   })
+  
+
+
+});//ready
